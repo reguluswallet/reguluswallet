67fa2108cefbd42ac0df7d2cf9751d44c1874269
Joshua Payne <suxur@me.com>
2018-03-01 00:12:07 -0600

    Convert common Button to LoadingButton

--- components/common/LoadingButton.js
+++ components/common/LoadingButton.js
@@ -1,24 +1,24 @@
 import React, {Component} from "react";
 import {ActivityIndicator} from "react-native";
-import {Button as NBButton, Text} from "native-base";
+import {Button, Text} from "native-base";
 import {Colors} from "../../constants";
 import {connect} from "react-redux";
 
-class ButtonComponent extends Component {
+class LoadingButtonComponent extends Component {
     renderButtonChild() {
         if (this.props.loading) {
-             return <ActivityIndicator color={Colors.white}/>
+            return <ActivityIndicator color={Colors.white}/>
         }
 
         return <Text>{this.props.children}</Text>;
     }
 
     render() {
-    return (
-        <NBButton {...this.props} disabled={this.props.loading}>
-            {this.renderButtonChild()}
-        </NBButton>
-    );
+        return (
+            <Button {...this.props} disabled={this.props.loading}>
+                {this.renderButtonChild()}
+            </Button>
+        );
     }
 }
 
@@ -27,6 +27,6 @@ const mapStateToProps = ({app}) => {
     return {loading};
 };
 
-const Button = connect(mapStateToProps)(ButtonComponent);
+const LoadingButton = connect(mapStateToProps)(LoadingButtonComponent);
 
-export {Button};
+export {LoadingButton};
--- components/index.js
+++ components/index.js
@@ -5,6 +5,6 @@ export * from './LogoImage';
 export * from './Transaction/Amount';
 export * from './common/Input';
 export * from './common/InputInfo';
-export * from './common/Button';
+export * from './common/LoadingButton';
 export * from './common/Text';
 export * from './SlideBackground';
