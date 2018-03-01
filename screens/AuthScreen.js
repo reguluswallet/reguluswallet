60e5c72bc698ea26f8ecb2fcdda477c4d75d184a
Joshua Payne <suxur@me.com>
2018-03-01 00:14:22 -0600

    Add Toast errors and remove anonymous login

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,14 +1,9 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
-import {
-    Animated,
-    Easing,
-    StyleSheet,
-    View,
-} from "react-native";
-import {Button, Content, Container, Text, Item, Input} from 'native-base';
-import {Login, LoginAnonymously, Register} from '../actions';
-import {Logo} from '../components';
+import {Animated, Easing, StyleSheet, View} from "react-native";
+import {Button, Container, Content, Input, Item, Text, Toast} from 'native-base';
+import {Login, Register, ToggleLoading} from '../actions';
+import {LoadingButton, Logo} from '../components';
 import {Colors, Layout} from '../constants';
 
 class AuthComponent extends Component {
@@ -20,41 +15,51 @@ class AuthComponent extends Component {
         animate_view: new Animated.Value(0),
         login_email: undefined,
         login_password: undefined,
-        login_error: false,
         register_email: undefined,
         register_password: undefined,
         register_confirm_password: undefined,
-        register_error: false
     };
 
+    showError(message) {
+        Toast.show({
+            text: message,
+            position: 'top',
+            type: 'danger'
+        });
+    }
+
     _register() {
-        let {register_email, register_password, register_confirm_password} = this.state;
+        let {register_email, register_password} = this.state;
+
+        const vm = this;
         if (this.validateRegister()) {
-            this.props.Register(register_email, register_password);
+            this.props.Register(register_email, register_password)
+                .catch((error) => {
+                    vm.showError(error);
+                })
         }
     }
 
     validateRegister() {
         let {register_email, register_password, register_confirm_password} = this.state;
-        this.setState({register_error: false});
 
         if (!register_email) {
-            this.setState({register_error: true});
+            this.showError('Must provide an e-mail address');
             return false;
         }
 
         if (!register_password) {
-            this.setState({register_error: true});
+            this.showError('Must provide a password');
             return false;
         }
 
         if (!register_confirm_password) {
-            this.setState({register_error: true});
+            this.showError('Must confirm password');
             return false;
         }
 
         if (register_password !== register_confirm_password) {
-            this.setState({register_error: true});
+            this.showError('Passwords must match');
             return false;
         }
 
@@ -77,22 +82,25 @@ class AuthComponent extends Component {
     _login() {
         let {login_email, login_password} = this.state;
 
+        const vm = this;
         if (this.validateLogin()) {
-            this.props.Login(login_email, login_password);
+            this.props.Login(login_email, login_password)
+                .catch((error) => {
+                    vm.showError(error);
+                })
         }
     }
 
     validateLogin() {
         let {login_email, login_password} = this.state;
-        this.setState({login_error: false});
 
         if (!login_email) {
-            this.setState({login_error: true});
+            this.showError('Must provide an e-mail address');
             return false;
         }
 
         if (!login_password) {
-            this.setState({login_error: true});
+            this.showError('Must provide a password');
             return false;
         }
 
@@ -119,7 +127,7 @@ class AuthComponent extends Component {
                     <Logo/>
                     <View style={styles.auth}>
                         <Animated.View style={[styles.loginContainer, {left: this.state.animate_view}]}>
-                            <Item regular top error={this.state.login_error}>
+                            <Item regular top>
                                 <Input
                                     top
                                     ref="LoginEmailInput"
@@ -132,6 +140,7 @@ class AuthComponent extends Component {
                                     placeholder="E-mail Address"
                                     placeholderTextColor={Colors.grey}
                                     onChangeText={email => this.setState({login_email: email})}
+                                    value={this.state.login_email}
                                     onSubmitEditing={() => {
                                         this.refs.PasswordInput.wrappedInstance.focus()
                                     }}
@@ -144,16 +153,14 @@ class AuthComponent extends Component {
                                     secureTextEntry
                                     selectionColor={Colors.tintColor}
                                     onChangeText={password => this.setState({login_password: password})}
+                                    value={this.state.login_password}
                                     placeholder="Password"
                                     placeholderTextColor={Colors.grey}
                                 />
                             </Item>
-                            <Button block onPress={this._login.bind(this)}>
-                                <Text>Login</Text>
-                            </Button>
-                            <Button block onPress={this.props.LoginAnonymously}>
-                                <Text>Login Anonymously</Text>
-                            </Button>
+                            <LoadingButton block onPress={this._login.bind(this)}>
+                                Login
+                            </LoadingButton>
                             <Button block transparent dark onPress={this._goToRegisterView.bind(this)}>
                                 <Text>Need An account? Register Here</Text>
                             </Button>
@@ -196,9 +203,9 @@ class AuthComponent extends Component {
                                     placeholderTextColor={Colors.grey}
                                 />
                             </Item>
-                            <Button block onPress={this._register.bind(this)}>
-                                <Text>Register</Text>
-                            </Button>
+                            <LoadingButton block onPress={this._register.bind(this)}>
+                                Register
+                            </LoadingButton>
                             <Button block transparent dark onPress={this._goToLoginView.bind(this)}>
                                 <Text>Already Have An Account? Login Here</Text>
                             </Button>
@@ -214,7 +221,7 @@ const mapStateToProps = state => ({
     state
 });
 
-const AuthScreen = connect(mapStateToProps, {Login, LoginAnonymously, Register})(AuthComponent);
+const AuthScreen = connect(mapStateToProps, {Login, Register, ToggleLoading})(AuthComponent);
 
 export {AuthScreen};
 
