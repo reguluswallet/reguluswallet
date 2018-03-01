13ca4c2af303f31531e27abdf39de5d502fe8fae
Joshua Payne <suxur@me.com>
2018-03-01 00:12:33 -0600

    Format some NativeBase components

--- native-base-theme/components/H3.js
+++ native-base-theme/components/H3.js
@@ -1,11 +1,12 @@
 import variable from "./../variables/platform";
 
 export default (variables = variable) => {
-  const h3Theme = {
-    color: variables.textColor,
-    fontSize: variables.fontSizeH3,
-    lineHeight: variables.lineHeightH3
-  };
+    const h3Theme = {
+        color: variables.textColor,
+        fontSize: variables.fontSizeH3,
+        lineHeight: variables.lineHeightH3,
+        fontFamily: 'clear-sans'
+    };
 
-  return h3Theme;
+    return h3Theme;
 };
