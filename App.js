cf58218eda88dcabf5e4f875fcac2505f28a620f
Joshua Payne <suxur@me.com>
2018-03-12 22:44:45 -0500

    Move Sentry config out and format file

--- App.js
+++ App.js
@@ -1,24 +1,23 @@
-import './global';
-import React, {Component} from 'react';
-import {NavigationActions} from 'react-navigation';
-import {AppState, StatusBar} from 'react-native';
-import {Provider} from 'react-redux';
-import {AppLoading, Asset, Font} from 'expo';
-import {Ionicons} from '@expo/vector-icons';
-import {Root, StyleProvider} from 'native-base';
-import getTheme from './native-base-theme/components'
-import firebase from 'firebase';
-import RootNavigation from './navigation/RootNavigation';
-import Config from './config';
-import {PersistGate} from 'redux-persist/integration/react'
-import {persistor, store} from './store';
-import Sentry from 'sentry-expo';
-import {LOCK, UNLOCK} from "./constants/types";
-
+import "./global";
+import React, { Component } from "react";
+import { NavigationActions } from "react-navigation";
+import { AppState, StatusBar } from "react-native";
+import { Provider } from "react-redux";
+import { AppLoading, Asset, Font } from "expo";
+import { Ionicons } from "@expo/vector-icons";
+import { Root, StyleProvider } from "native-base";
+import firebase from "firebase";
+import Sentry from "sentry-expo";
+import { PersistGate } from "redux-persist/integration/react";
+import RootNavigation from "./navigation/RootNavigation";
+import getTheme from "./native-base-theme/components";
+import Config from "./config";
+import { persistor, store } from "./store";
+import { LOCK, UNLOCK } from "./constants/types";
 
 Sentry.enableInExpoDevelopment = true;
 // import { SentrySeverity, SentryLog } from 'react-native-sentry';
-Sentry.config('https://0640d0a03ec34f9b8c810ba3b3b341fe@sentry.io/290824').install();
+Sentry.config(Config.sentry.url).install();
 
 firebase.initializeApp(Config.firebase);
 
@@ -30,31 +29,32 @@ export default class App extends Component {
     };
 
     componentDidMount() {
-        AppState.addEventListener('change', this._handleAppStateChange);
+        AppState.addEventListener("change", this._handleAppStateChange);
     }
 
     componentWillUnmount() {
-        AppState.removeEventListener('change', this._handleAppStateChange);
+        AppState.removeEventListener("change", this._handleAppStateChange);
     }
 
-    _handleAppStateChange = (nextAppState) => {
-        if (this.state.appState.match(/inactive|background/) && nextAppState === 'active') {
+    _handleAppStateChange = nextAppState => {
+        if (
+            this.state.appState.match(/inactive|background/) &&
+            nextAppState === "active"
+        ) {
             // If TouchID is available and enabled?
             // If pin is set?
-            store.dispatch({type: UNLOCK});
-            store.dispatch(
-                NavigationActions.back()
-            )
+            store.dispatch({ type: UNLOCK });
+            store.dispatch(NavigationActions.back());
+        } else {
+            if (!store.getState().app.locked) {
+                store.dispatch({ type: LOCK });
+                store.dispatch(
+                    NavigationActions.navigate({ routeName: "Splash" })
+                );
+            }
         }
-        else {
-            store.dispatch({type: LOCK});
-            store.dispatch(
-                NavigationActions.navigate({
-                    routeName: 'Splash'
-                }))
 
-        }
-        this.setState({appState: nextAppState});
+        this.setState({ appState: nextAppState });
     };
 
     render() {
@@ -72,8 +72,8 @@ export default class App extends Component {
                     <PersistGate loading={null} persistor={persistor}>
                         <StyleProvider style={getTheme()}>
                             <Root>
-                                <StatusBar barStyle="default"/>
-                                <RootNavigation/>
+                                <StatusBar barStyle="default" />
+                                <RootNavigation />
                             </Root>
                         </StyleProvider>
                     </PersistGate>
@@ -85,16 +85,16 @@ export default class App extends Component {
     _loadResourcesAsync = async () => {
         return Promise.all([
             Asset.loadAsync([
-                require('./assets/images/wallet.png'),
-                require('./assets/images/network.png'),
-                require('./assets/images/send-receive.png')
+                require("./assets/images/wallet.png"),
+                require("./assets/images/network.png"),
+                require("./assets/images/send-receive.png")
             ]),
             Font.loadAsync({
                 // This is the font that we are using for our tab bar
                 ...Ionicons.font,
-                'clear-sans': require('./assets/fonts/ClearSans-Regular.ttf'),
-                'clear-sans-bold': require('./assets/fonts/ClearSans-Bold.ttf'),
-            }),
+                "clear-sans": require("./assets/fonts/ClearSans-Regular.ttf"),
+                "clear-sans-bold": require("./assets/fonts/ClearSans-Bold.ttf")
+            })
         ]);
     };
 
@@ -105,6 +105,6 @@ export default class App extends Component {
     };
 
     _handleFinishLoading = () => {
-        this.setState({isLoadingComplete: true});
+        this.setState({ isLoadingComplete: true });
     };
 }
